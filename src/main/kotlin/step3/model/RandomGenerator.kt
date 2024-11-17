6aea62ddd50390fef2f80cf5544f2c1ca2ed45ae
이민수 <mns0304@cashwalk.io>
2024-11-18 00:52:13 +0900

    step3, RandomGenerator 추가

---
+++ src/main/kotlin/step3/model/RandomGenerator.kt
@@ -0,0 +1,17 @@
+package step3.model
+
+interface RandomGenerator {
+    fun generator(range: IntRange): Int
+}
+
+object RandomGeneratorFactory {
+    fun newInstance(): RandomGenerator {
+        return RandomGeneratorImpl()
+    }
+}
+
+private class RandomGeneratorImpl: RandomGenerator {
+    override fun generator(range: IntRange): Int {
+        return range.random()
+    }
+}
