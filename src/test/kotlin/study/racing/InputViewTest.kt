ab8205a97c96052f2812ad7138ea9eeab8209684
이민수 <mns0304@cashwalk.io>
2024-11-18 00:51:45 +0900

    step3, 자동차 레이싱 InputView 생성 및 Test

---
+++ src/test/kotlin/study/racing/InputViewTest.kt
@@ -0,0 +1,55 @@
+package study.racing
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import step3.view.InputViewFactory
+
+class InputViewTest {
+    @Test
+    fun `숫자 입력값 검증 테스트`() {
+        val inputView = InputViewFactory.newInstance()
+        var carCountStr: String? = ""
+        var exception = assertThrows<IllegalArgumentException> {
+            inputView.inputNumber(carCountStr)
+        }
+        assert(exception.message == "입력값을 확인해주세요.")
+
+        carCountStr = null
+        exception = assertThrows<IllegalArgumentException> {
+            inputView.inputNumber(carCountStr)
+        }
+        assert(exception.message == "입력값을 확인해주세요.")
+
+        carCountStr = "="
+        exception = assertThrows<IllegalArgumentException> {
+            inputView.inputNumber(carCountStr)
+        }
+        assert(exception.message == "숫자를 입력해주세요.")
+
+        carCountStr = "1 2"
+        exception = assertThrows<IllegalArgumentException> {
+            inputView.inputNumber(carCountStr)
+        }
+        assert(exception.message == "숫자를 입력해주세요.")
+
+        carCountStr = "0"
+        exception = assertThrows<IllegalArgumentException> {
+            inputView.inputNumber(carCountStr)
+        }
+        assert(exception.message == "최소 0보다 큰수를 입력해주세요.")
+
+        carCountStr = "-5"
+        exception = assertThrows<IllegalArgumentException> {
+            inputView.inputNumber(carCountStr)
+        }
+        assert(exception.message == "최소 0보다 큰수를 입력해주세요.")
+
+        carCountStr = "10"
+        assertThat(inputView.inputNumber(carCountStr))
+            .isGreaterThan(0)
+
+        assertThat(inputView.inputNumber(carCountStr))
+            .isEqualTo(10)
+    }
+}
