c0ebb9eebf7958e8561193bd376d900c9d1ed505
이민수 <mns0304@cashwalk.io>
2024-11-25 20:46:36 +0900

    step5, 코드 흐름 개선을 위해 공개 메서드를 상단으로 배치

--- src/main/kotlin/racing/view/input/ConsoleInputView.kt
+++ src/main/kotlin/racing/view/input/ConsoleInputView.kt
@@ -1,41 +1,14 @@
 package racing.view.input
 
 internal class ConsoleInputView : InputView {
-    private fun displayCarNamesQuestion() {
-        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
-    }
-
-    private fun displayAttemptCountQuestion() {
-        println("시도할 횟수는 몇 회인가요?")
-    }
-
-    private fun inputCarNames(input: String?): List<String> {
-        val validatedInput = validateCarNamesInput(input)
-        return validatedInput.split(DELIMITER_COMMA).map { it.trim() }
-    }
-
-    private fun inputNumber(input: String?): Int {
-        return validateIntInput(input).toInt()
+    override fun promptAndValidateCarNamesInput(): List<String> {
+        return promptForCharNames(::displayCarNamesQuestion)
     }
 
     private fun promptForCharNames(questionBlock: () -> Unit): List<String> {
         questionBlock()
         val inputCarNames = validateAndGetCarNames(questionBlock = questionBlock)
         return inputCarNames
-    }
-
-    private fun promptForNumberInput(questionBlock: () -> Unit): Int {
-        questionBlock()
-        val inputNumber = validateAndGetNumber(questionBlock = questionBlock)
-        return inputNumber
-    }
-
-    override fun promptAndValidateCarNamesInput(): List<String> {
-        return promptForCharNames(::displayCarNamesQuestion)
-    }
-
-    override fun promptAndValidateAttemptCountInput(): Int {
-        return promptForNumberInput(::displayAttemptCountQuestion)
     }
 
     private fun validateAndGetCarNames(
@@ -50,6 +23,33 @@ internal class ConsoleInputView : InputView {
         }
     }
 
+    private fun inputCarNames(input: String?): List<String> {
+        val validatedInput = validateCarNamesInput(input)
+        return validatedInput.split(DELIMITER_COMMA).map { it.trim() }
+    }
+
+    private fun validateCarNamesInput(input: String?): String {
+        require(!input.isNullOrEmpty()) { "입력값을 확인해주세요." }
+        require(input.split(DELIMITER_COMMA).none { it.trim().length > 5 }) {
+            "자동차 이름은 ${DEFAULT_NAME_MAX}자를 초과할 수 없습니다."
+        }
+        return input
+    }
+
+    private fun displayCarNamesQuestion() {
+        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+    }
+
+    override fun promptAndValidateAttemptCountInput(): Int {
+        return promptForNumberInput(::displayAttemptCountQuestion)
+    }
+
+    private fun promptForNumberInput(questionBlock: () -> Unit): Int {
+        questionBlock()
+        val inputNumber = validateAndGetNumber(questionBlock = questionBlock)
+        return inputNumber
+    }
+
     private fun validateAndGetNumber(
         input: String? = readlnOrNull(),
         questionBlock: () -> Unit,
@@ -62,12 +62,8 @@ internal class ConsoleInputView : InputView {
         }
     }
 
-    private fun validateCarNamesInput(input: String?): String {
-        require(!input.isNullOrEmpty()) { "입력값을 확인해주세요." }
-        require(input.split(DELIMITER_COMMA).none { it.trim().length > 5 }) {
-            "자동차 이름은 ${DEFAULT_NAME_MAX}자를 초과할 수 없습니다."
-        }
-        return input
+    private fun inputNumber(input: String?): Int {
+        return validateIntInput(input).toInt()
     }
 
     private fun validateIntInput(input: String?): String {
@@ -75,6 +71,10 @@ internal class ConsoleInputView : InputView {
         require(input.toIntOrNull() != null) { "숫자를 입력해주세요." }
         require(input.toInt() > 0) { "최소 0보다 큰수를 입력해주세요." }
         return input
+    }
+
+    private fun displayAttemptCountQuestion() {
+        println("시도할 횟수는 몇 회인가요?")
     }
 
     companion object {
