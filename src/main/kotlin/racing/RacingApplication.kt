0d4fd8835e20e0fd059b9a572338a72f43c84fe1
이민수 <mns0304@cashwalk.io>
2024-11-25 20:40:31 +0900

    step5, 타입 명시 생략

--- src/main/kotlin/racing/RacingApplication.kt
+++ src/main/kotlin/racing/RacingApplication.kt
@@ -2,13 +2,11 @@ package racing
 
 import racing.controller.RacingControllerFactory
 import racing.view.input.ConsoleInputViewFactory
-import racing.view.input.InputView
 import racing.view.result.ConsoleResultViewFactory
-import racing.view.result.ResultView
 
 fun main() {
-    val inputView: InputView = ConsoleInputViewFactory.newInstance()
-    val resultView: ResultView = ConsoleResultViewFactory.newInstance()
+    val inputView = ConsoleInputViewFactory.newInstance()
+    val resultView = ConsoleResultViewFactory.newInstance()
     val racingController = RacingControllerFactory.newInstance(inputView, resultView)
     racingController.start()
 }
